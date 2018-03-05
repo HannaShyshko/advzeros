3633d2fbc2932a5054747ba40cd5dda4645109a3
Hanna <hannashishko@gmail.com>
2018-03-05 07:04:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
+module.exports = function getZerosCount(number, base) {
+ 
+var zero = number;
+  
+  for (var i = 2; i <= base; i++) {
+      var a = 0;
+      var b = number;
 
+      while (base % i == 0) {
+      	var c = 0;
+        c++;
+        base = Math.floor(base/i);
+      
+      while (b/i > 0) { 
+        a += Math.floor(b/i);
+        b = Math.floor(b/i);
+      }
+      zero = Math.floor(Math.min(zero, a/c));
+    }
+  }
+  return zero;
+}
